2fdc0a6b20ee88ee02fd1a8c6573dd504bda604a
François Vieille <vieille.francois@gmail.com>
2018-10-23 16:15:28 +0200

    Heritage car plusieurs loader + modele possibles en vue

--- src/models/WordDataLoader.py
+++ src/models/WordDataLoader.py
@@ -3,52 +3,24 @@ from keras.utils import to_categorical
 
 class WordDataLoader:
 
-	def __init__(self, load_type, num_steps, skip_step, word_data, batch_size, step_per_epoch = None):
+	def __init__(self, num_steps, skip_step, word_data, batch_size, step_per_epoch = None):
+
 		self.num_steps = num_steps
 		self.skip_step = skip_step
 		self.word_data = word_data
 		self.batch_size = batch_size
 		self.step_per_epoch = step_per_epoch
 		self.current_idx = 0
-		self.type = load_type
+
+	def stepPerEpoch(self):
+		if self.step_per_epoch is None:
+			return len(self.word_data.token_final) // self.batch_size
+		return self.step_per_epoch
+
+
+class IdWordDataLoader(WordDataLoader):
 
 	def generate(self):
-		if self.type == 'id':
-			return self.generateId()
-		elif self.type == 'array':
-			return self.generateArray()
-
-
-	def generateArray(self):
-
-		max_len = max([len(el) for el in self.word_data.token_final])
-
-		while True:
-			X = np.zeros((self.batch_size, max_len, self.word_data.getVocabularyLength()))
-			Y = np.zeros((self.batch_size, max_len, self.word_data.getVocabularyLength()))
-
-			for i in range(self.batch_size):
-				if self.current_idx >= len(self.word_data.token_final):
-					self.current_idx = 0
-
-				x = self.word_data.token_final[self.current_idx][:-1]
-				x = [self.word_data.ref_word_to_id[word] for word in x]
-				x = to_categorical(x, num_classes=self.word_data.getVocabularyLength())
-				X[i, :(x.shape[0]), : ] = x
-
-				y = self.word_data.token_final[self.current_idx][1:]
-				y = [self.word_data.ref_word_to_id[word] for word in y]
-				y = to_categorical(y, num_classes=self.word_data.getVocabularyLength())
-				Y[i, :(y.shape[0]), : ] = y
-				
-				# idx_debut = self.current_idx*self.batch_size:
-				# idx_fin = idx_debut + (self.batch_size) - 1
-
-				self.current_idx += 1
-
-			yield X, Y
-
-	def generateId(self):
 		max_len = max([len(el) for el in self.word_data.token_final])
 
 		tokens_flat = self.word_data.getFlatFinalToken()
@@ -76,7 +48,33 @@ class WordDataLoader:
 			yield X, Y
 
 
-	def stepPerEpoch(self):
-		if self.step_per_epoch is None:
-			return len(self.word_data.token_final) // self.batch_size
-		return self.step_per_epoch
+class OneHotIdWordDataLoader(WordDataLoader):
+
+	def generate(self):
+
+		max_len = max([len(el) for el in self.word_data.token_final])
+
+		while True:
+			X = np.zeros((self.batch_size, max_len, self.word_data.getVocabularyLength()))
+			Y = np.zeros((self.batch_size, max_len, self.word_data.getVocabularyLength()))
+
+			for i in range(self.batch_size):
+				if self.current_idx >= len(self.word_data.token_final):
+					self.current_idx = 0
+
+				x = self.word_data.token_final[self.current_idx][:-1]
+				x = [self.word_data.ref_word_to_id[word] for word in x]
+				x = to_categorical(x, num_classes=self.word_data.getVocabularyLength())
+				X[i, :(x.shape[0]), : ] = x
+
+				y = self.word_data.token_final[self.current_idx][1:]
+				y = [self.word_data.ref_word_to_id[word] for word in y]
+				y = to_categorical(y, num_classes=self.word_data.getVocabularyLength())
+				Y[i, :(y.shape[0]), : ] = y
+				
+				# idx_debut = self.current_idx*self.batch_size:
+				# idx_fin = idx_debut + (self.batch_size) - 1
+
+				self.current_idx += 1
+
+			yield X, Y
--- src/models/simpleLSTMModel.py
+++ src/models/simpleLSTMModel.py
@@ -2,19 +2,31 @@ from keras.models import Sequential, load_model
 from keras.layers import Dense, Activation, Embedding, Dropout, TimeDistributed
 from keras.layers import LSTM
 
+class AbstractModel:
+	def __init__(self, save_directory):
+		self.save_directory = save_directory
 
-class simpleLSTMModel:
+	def save(self):
+		self.model.save('{}/final_model.hdf5'.format(self.save_directory))
 
-	def __init__(self, vocabulary_size, lstm_hidden_size, batch_input_shape):
+
+	def load(self, which = 'final_model'):
+		self.model = load_model('{}/{}.hdf5'.format(self.save_directory, which))
+
+
+class simpleLSTMModel(AbstractModel):
+
+	def __init__(self, save_directory, vocabulary_size, lstm_hidden_size, batch_input_shape):
+
+		super(self, save_directory)
 
 		self.vocabulary_size = vocabulary_size
 		self.lstm_hidden_size = lstm_hidden_size
 		self.batch_input_shape = batch_input_shape
 		self.build_model()
-		self.save_directory = 'models/simpleLSTMModel/version1'
+
 
 	def build_model(self):
-		# batch_input_length=self.batch_input_shape.
 
 		model = Sequential()
 		model.add(Embedding(input_dim = self.vocabulary_size, output_dim = self.lstm_hidden_size, batch_input_shape = self.batch_input_shape))
@@ -28,17 +40,7 @@ class simpleLSTMModel:
 
 		return self.model
 
-	def save(self):
-		self.model.save('{}/final_model.hdf5'.format(self.save_directory))
 
-
-	def load(self, which = 'final_model'):
-		self.model = load_model('{}/{}.hdf5'.format(self.save_directory, which))
-
-	def checkpointer(self):
-		ModelCheckpoint(filepath='models/simpleLSTMModel/version1/model-{epoch:02d}.hdf5', verbose = 1, period = 5)
-
-			
 
 
 

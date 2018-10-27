084939f74a4fd7c0032e3c43553a2f7f74f380ed
François Vieille <vieille.francois@gmail.com>
2018-10-27 03:07:31 +0200

    augmentation jeu de donnee

--- src/process_simple_lstm.py
+++ src/process_simple_lstm.py
@@ -41,7 +41,7 @@ def main(goal, tokens_dir, save_dir, model_dir, nb_token_keep, lstm_hidden_size,
 
 	## ------ lecture des tweets.csv ------
 	tweets_tokens = pd.read_csv('{}/tweets_2.csv'.format(tokens_dir))
-	tokens_ss_pbm = tweets_tokens[(tweets_tokens.count_hashtag_ano == 0)  & (tweets_tokens.count_mention_ano == 0)].index
+	tokens_ss_pbm = tweets_tokens[(tweets_tokens.count_hashtag_ano <= 2)  & (tweets_tokens.count_mention_ano <= 1)].index
 
     ## ------ lecture des tokens à garder ------
 	with open('{}/tokens_id_keep_2.json'.format(tokens_dir)) as f:
